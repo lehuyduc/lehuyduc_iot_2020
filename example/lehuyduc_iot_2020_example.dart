a8980a4d319fcde27a25649bcce2ac9b61cf21e4
lehuyduc <lehuyduc3@gmail.com>
2020-12-18 21:18:50 +0700

    first commit

---
+++ example/lehuyduc_iot_2020_example.dart
@@ -0,0 +1,6 @@
+import 'package:lehuyduc_iot_2020/lehuyduc_iot_2020.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/lehuyduc_iot_2020_test.dart
@@ -0,0 +1,16 @@
+import 'package:lehuyduc_iot_2020/lehuyduc_iot_2020.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    Awesome awesome;
+
+    setUp(() {
+      awesome = Awesome();
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
